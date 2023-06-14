e73b25295ebabc760b07e2b551bbabbe88d97abd
Max Burleigh <webwavebuilding@yahoo.com>
2023-06-13 21:10:40 -0700

    Fixed mobile hero box

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,14 +13,14 @@ function Home() {
         style={{
           backgroundImage: `url(${HeroImage})`,
         }}>
-        <div className="bg-white bg-opacity-10 backdrop-blur-lg rounded-xl shadow-inner p-6 text-center text-white px-6 border-[1px] border-white drop-shadow-xl backdrop-brightness-75">
-          <h1 className="text-5xl md:text-6xl mb-6 drop-shadow-md font-aoboshi">
+        <div className="bg-white bg-opacity-10 backdrop-blur-lg rounded-xl shadow-inner p-4 sm:p-6 text-center text-white px-4 sm:px-6 border-[1px] border-white drop-shadow-xl backdrop-brightness-75">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl mb-4 sm:mb-6 drop-shadow-md font-aoboshi">
             genesis
           </h1>
-          <h2 className="text-4xl mb-4 font-bold drop-shadow-md">
+          <h2 className="text-3xl sm:text-4xl mb-3 sm:mb-4 font-bold drop-shadow-md">
             Tech Install/Support
           </h2>
-          <p className="text-xl md:text-2xl font-light mb-8 drop-shadow-2xl">
+          <p className="text-lg sm:text-xl md:text-2xl font-light mb-6 sm:mb-8 drop-shadow-2xl">
             Your one-stop solution for all your tech needs.
           </p>
           <MotionLink
@@ -28,7 +28,7 @@ function Home() {
               scale: 1.2,
               transition: { duration: 0.1, ease: "linear" },
             }}
-            className="inline-block py-4 px-8 text-xl bg-genesis-orange text-white font-bold rounded-full transition duration-300 hover:bg-orange-700 drop-shadow-md"
+            className="inline-block py-2 sm:py-4 px-6 sm:px-8 text-lg sm:text-xl bg-genesis-orange text-white font-bold rounded-full transition duration-300 hover:bg-orange-700 drop-shadow-md"
             to="/subscription">
             Subscribe Now
           </MotionLink>
@@ -36,10 +36,12 @@ function Home() {
       </section>
 
       <section className="bg-white py-20">
-        <div className="container mx-auto px-8">
-          <h2 className="text-3xl font-bold text-center mb-8">Our Services</h2>
-          <div className="max-w-xl mx-auto">
-            <p className="text-lg text-center mb-8">
+        <div className="container mx-auto px-4 sm:px-8">
+          <h2 className="text-2xl sm:text-3xl font-bold text-center mb-6 sm:mb-8">
+            Our Services
+          </h2>
+          <div className="max-w-sm sm:max-w-xl mx-auto">
+            <p className="text-base sm:text-lg text-center mb-6 sm:mb-8">
               Whether it's one-on-one tech education, or mounting and installing
               your new smart TV, or even troubleshooting your PC & Mac software,
               we've got you covered. We offer a wide range of tech support
@@ -48,9 +50,9 @@ function Home() {
               services for our non-members.
             </p>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-3xl mx-auto">
+          <div className="grid grid-cols-1 gap-4 sm:gap-8 max-w-xs sm:max-w-3xl mx-auto">
             <div className="text-center">
-              <h3 className="text-2xl mb-4 font-bold">
+              <h3 className="text-xl sm:text-2xl mb-3 sm:mb-4 font-bold">
                 One-On-One Tech Education
               </h3>
               <p>
@@ -59,14 +61,16 @@ function Home() {
               </p>
             </div>
             <div className="text-center">
-              <h3 className="text-2xl mb-4 font-bold">TV Mounting & Setup</h3>
+              <h3 className="text-xl sm:text-2xl mb-3 sm:mb-4 font-bold">
+                TV Mounting & Setup
+              </h3>
               <p>
                 From unboxing your new smart TV to mounting it and setting it up
                 with your devices, we've got you covered.
               </p>
             </div>
             <div className="text-center">
-              <h3 className="text-2xl mb-4 font-bold">
+              <h3 className="text-xl sm:text-2xl mb-3 sm:mb-4 font-bold">
                 PC & Mac Software Troubleshooting
               </h3>
               <p>
@@ -75,7 +79,9 @@ function Home() {
               </p>
             </div>
             <div className="text-center">
-              <h3 className="text-2xl mb-4 font-bold">In-Home Tech Support</h3>
+              <h3 className="text-xl sm:text-2xl mb-3 sm:mb-4 font-bold">
+                In-Home Tech Support
+              </h3>
               <p>
                 With our in-home tech support service, you can enjoy a
                 convenient, hands-off solution to all your tech problems.
