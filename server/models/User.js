82a78aa4ce7da3cf25fb1732a75cc3fd60d035ff
Max Burleigh <webwavebuilding@yahoo.com>
2023-07-08 20:45:24 -0700

    removed unnecessary hashing in user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
+// User Model
 // Import mongoose and bcryptjs
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
 
 // Define the User schema
 const UserSchema = new mongoose.Schema({
@@ -21,20 +21,6 @@ const UserSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-});
-
-// Before we save a user to the database
-UserSchema.pre("save", async function (next) {
-  const user = this; // 'this' refers to the user object
-
-  // If the password is modified (or this is a new user)
-  if (user.isModified("password")) {
-    // Hash the password with bcrypt and replace the plain-text password with the hashed one
-    user.password = await bcrypt.hash(user.password, 8);
-  }
-
-  // Proceed to the next step (saving the user)
-  next();
 });
 
 // Create the User model based on the schema and export it
