00b1c50fe5c05d5727e14329ac9270f28bb42779
Max Burleigh <webwavebuilding@yahoo.com>
2023-07-09 01:07:01 -0700

    added firstName to joi

--- server/server.js
+++ server/server.js
@@ -47,11 +47,12 @@ app.use(express.json());
 // Validation schemas
 const registerSchema = Joi.object({
   username: Joi.string().alphanum().min(3).max(30).required(),
-  password: Joi.string().min(3).max(30).required(),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
   email: Joi.string().email({
     minDomainSegments: 2,
     tlds: { allow: ["com", "net"] },
   }),
+  firstName: Joi.string().min(1).max(30).required(),
 });
 
 const loginSchema = Joi.object({
