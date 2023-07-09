da78be487b6b1e3de8fb2efb20e8c94c5fa8b7ea
Max Burleigh <webwavebuilding@yahoo.com>
2023-07-08 19:46:58 -0700

    updated auth context file

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,35 +1,26 @@
 import React, { createContext, useState } from "react";
 import { login, register, logout } from "../services/auth";
-/**
- * Creating a context for auth-related aspects
- * Context provides a way to pass data through the component tree without having to pass props down manually at every level
- */
+
 export const AuthContext = createContext();
 
-/**
- * AuthProvider is a custom context provider for handling user authentications (login, registration, logout).
- * This will wrap the components which interact with user authentication data such as login/reg state, error state
- */
 export const AuthProvider = ({ children }) => {
-  // user state variable and its updater setUser
-  // it retrieves user data from local storage, if any, during initial load (refresh or first load)
   const [user, setUser] = useState(() => {
     const localUser = localStorage.getItem("user");
-    // check if localUser is not "undefined"
     return localUser && localUser !== "undefined"
       ? JSON.parse(localUser)
       : null;
   });
 
-  // error state to handle any auth-related errors
   const [error, setError] = useState(null);
 
-  // loginCallback is our custom callback for login that accepts username, password as arguments
-  const loginCallback = (username, password) =>
-    login(username, password, setUser, setError);
+  const loginCallback = async (username, password) => {
+    const result = await login(username, password, setUser, setError);
+    if (result) {
+      setUser(result);
+    }
+  };
 
-  // registerCallback is our custom callback for register that accepts all input fields (username, email, password, firstName etc.) as arguments
-  const registerCallback = (
+  const registerCallback = async (
     username,
     email,
     password,
@@ -38,8 +29,8 @@ export const AuthProvider = ({ children }) => {
     address,
     phone,
     zipCode
-  ) =>
-    register(
+  ) => {
+    const result = await register(
       username,
       email,
       password,
@@ -51,15 +42,16 @@ export const AuthProvider = ({ children }) => {
       setUser,
       setError
     );
+    if (result) {
+      setUser(result);
+    }
+  };
 
-  // logoutCallback is our custom callback for logout
   const logoutCallback = () => logout(setUser);
 
-  // resetError is a function to set error null, to reset any occurred error
   const resetError = () => setError(null);
 
   return (
-    // Using context provider to provide auth-related data and functions to child components
     <AuthContext.Provider
       value={{
         user,
@@ -69,9 +61,7 @@ export const AuthProvider = ({ children }) => {
         error,
         resetError,
       }}>
-      {
-        children /*This represents child nodes/components wrapped by this provider*/
-      }
+      {children}
     </AuthContext.Provider>
   );
 };
