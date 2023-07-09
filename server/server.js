0dbe506c89b2e5e04215f497aa5401b1f4e2e121
Max Burleigh <webwavebuilding@yahoo.com>
2023-07-09 00:54:29 -0700

    adjusted validation schema

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.use(express.json());
 // Validation schemas
 const registerSchema = Joi.object({
   username: Joi.string().alphanum().min(3).max(30).required(),
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+  password: Joi.string().min(3).max(30).required(),
   email: Joi.string().email({
     minDomainSegments: 2,
     tlds: { allow: ["com", "net"] },
@@ -56,7 +56,7 @@ const registerSchema = Joi.object({
 
 const loginSchema = Joi.object({
   username: Joi.string().alphanum().min(3).max(30).required(),
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+  password: Joi.string().min(3).max(30).required(),
 });
 
 // your other routes here...
