920d194b28dec95cfe1e8e650b67e00e17445e07
Max Burleigh <webwavebuilding@yahoo.com>
2023-07-09 07:55:11 -0700

    adjusted registered schema

--- server/server.js
+++ server/server.js
@@ -47,12 +47,16 @@ app.use(express.json());
 // Validation schemas
 const registerSchema = Joi.object({
   username: Joi.string().alphanum().min(3).max(30).required(),
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
-  email: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: { allow: ["com", "net"] },
-  }),
+  password: Joi.string().pattern(new RegExp("^[ -~]{3,30}$")).required(),
+  email: Joi.string().email().required(),
   firstName: Joi.string().min(1).max(30).required(),
+  lastName: Joi.string().min(1).max(30).required(),
+  address: Joi.string().min(1).max(100).required(),
+  zipCode: Joi.string().min(5).max(9).required(),
+  phoneNumber: Joi.string()
+    .length(10)
+    .pattern(/^[0-9]+$/)
+    .required(),
 });
 
 const loginSchema = Joi.object({
