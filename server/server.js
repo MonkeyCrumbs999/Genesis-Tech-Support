4e854040c27646f8777bb76e9833fa03d08ca5d6
Max Burleigh <webwavebuilding@yahoo.com>
2023-07-12 21:15:51 -0700

    adjusted server code

--- server/server.js
+++ server/server.js
@@ -2,19 +2,31 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const session = require("express-session");
-const connectDB = require("./db");
-const { PORT, SESSION_SECRET, CORS_ORIGIN } = require("./config");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const { MONGODB_URI, SESSION_SECRET, PORT, CORS_ORIGIN } = require("./config");
 const passport = require("./auth");
 const userRoutes = require("./routes/userRoutes");
-const loggingMiddleware = require("./middleware/logging");
 
-connectDB();
+// Define MongoDB store
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "mySessions",
+});
 
-app.use(loggingMiddleware); // Use the logging middleware
+store.on("error", function (error) {
+  console.log(error);
+});
 
 app.use(
   session({
     secret: SESSION_SECRET,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+      secure: true, // set this to true in production
+      httpOnly: true, // The cookie is not accessible via client side script
+      sameSite: "none", // The cookie cannot be accessed by other sites
+    },
+    store: store, // using MongoDB session store
     resave: false,
     saveUninitialized: false,
   })
@@ -29,8 +41,10 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use(express.json());
 
+// Routes
 app.use("/user", userRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
