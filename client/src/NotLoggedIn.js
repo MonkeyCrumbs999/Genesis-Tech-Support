b7da40167a8e7dcbac42889694f7751819301c9b
Max Burleigh <webwavebuilding@yahoo.com>
2023-08-16 20:56:25 -0700

    adjusted not logged in screen

--- client/src/NotLoggedIn.js
+++ client/src/NotLoggedIn.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 
 export default function NotLoggedIn() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen p-4 pt-28">
+    <div className="flex flex-col items-center justify-center h-screen p-4 pt-20">
       <h2 className="text-3xl font-bold">You're not logged in!</h2>
       <p className="text-lg mt-4">
-        Please
-        <Link to="/login" className="text-blue-500 underline px-2">
+        Please&nbsp;
+        <Link to="/login" className="text-blue-500 underline">
           login
         </Link>
-        to view this page.
+        &nbsp;to view this page.
       </p>
     </div>
   );
