19497a386750a6ca6c97bad10626dfafe41cd53f
Max Burleigh <webwavebuilding@yahoo.com>
2023-08-23 11:58:58 -0700

    added css loading spinner and zoom on initial load

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { BrowserRouter as Router, useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import MobileSidebar from "./components/MobileSidebar";
 import Footer from "./components/Footer";
@@ -7,30 +7,15 @@ import ScrollProgressBar from "./components/ScrollProgressBar";
 import { AuthProvider } from "./contexts/AuthContext";
 import AppRoutes from "./routes";
 import "./App.css";
-
-function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    // Detect Microsoft Edge
-    const isEdge = window.navigator.userAgent.indexOf("Edg") > -1;
-
-    if (isEdge && 'scrollRestoration' in window.history) {
-      window.history.scrollRestoration = 'manual';
-    }
-
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-}
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-  const toggleMenu = useCallback(() => {
-    setIsOpen(prevIsOpen => !prevIsOpen);
-  }, []);
+  const toggleMenu = () => {
+    setIsOpen((prevIsOpen) => !prevIsOpen);
+  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -43,10 +28,26 @@ export default function App() {
     return () => window.removeEventListener("resize", handleResize);
   }, [isOpen]);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 500); // You can adjust the loading time as per your needs
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <CircularProgress />
+      </div>
+    );
+  }
+
   return (
+    <div className="zoomIn">
     <AuthProvider>
       <Router>
-        <ScrollToTop />
         <ScrollProgressBar />
         <Header toggleMenu={toggleMenu} />
         <div
@@ -61,5 +62,6 @@ export default function App() {
         <Footer />
       </Router>
     </AuthProvider>
+    </div>
   );
 }
