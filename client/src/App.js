10ae6a1c090d1ac6f30fd334280fcf05acfc8d2d
Max Burleigh <webwavebuilding@yahoo.com>
2023-09-03 11:15:56 -0700

    adjusted app css

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import MobileSidebar from "./components/MobileSidebar";
 import Footer from "./components/Footer";
 import ScrollProgressBar from "./components/ScrollProgressBar";
-import ScrollToTop from "./ScrollToTop";  // <-- Import here
+import ScrollToTop from "./ScrollToTop";
 import { AuthProvider } from "./contexts/AuthContext";
 import AppRoutes from "./routes";
 import "./App.css";
@@ -28,11 +28,11 @@ export default function App() {
   }, [isOpen]);
 
   return (
-  
-      <AuthProvider>
-        <Router>
-          <ScrollToTop />  {/* <-- Include here */}
-          <ScrollProgressBar />
+    <AuthProvider>
+      <Router>
+        <ScrollToTop />
+        <ScrollProgressBar />
+        <div className="flex flex-col min-h-screen">
           <Header toggleMenu={toggleMenu} />
           <div
             className={`fixed inset-0 bg-black transition-opacity duration-300 ease-in-out z-50 ${
@@ -42,9 +42,12 @@ export default function App() {
             }`}
           ></div>
           <MobileSidebar isOpen={isOpen} toggleMenu={toggleMenu} />
-          <AppRoutes />
+          <div className="flex-grow">
+            <AppRoutes />
+          </div>
           <Footer />
-        </Router>
-      </AuthProvider>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
--- client/src/components/Appointment.js
+++ client/src/components/Appointment.js
@@ -6,6 +6,7 @@ import { motion, AnimatePresence } from "framer-motion";
 function Appointment() {
   const [loading, setLoading] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [showRedirectMessage, setShowRedirectMessage] = useState(false); // Added state for redirect message
 
   const modalVariants = {
     hidden: { opacity: 0, scale: 0.8 },
@@ -13,9 +14,15 @@ function Appointment() {
     exit: { opacity: 0, scale: 0.8 },
   };
 
+  // Function to handle modal close
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setShowRedirectMessage(true); // Show redirect message when modal is closed
+  };
+
   return (
-    <MotionMain variants={fadeIn} initial="hidden" animate="visible" className="mb-48">
-      <main className="pt-28 p-4">
+    <MotionMain variants={fadeIn} initial="hidden" animate="visible">
+      <main className="pt-[10rem] p-4 min-h-[50vh]">
         <h2 className="text-4xl text-center mb-4">Schedule an Appointment</h2>
         <p className="text-center mb-4">
           Use the booking interface provided by Square below to schedule your appointment.
@@ -26,16 +33,23 @@ function Appointment() {
             onClick={() => {
               setShowModal(true);
               setLoading(true);
+              setShowRedirectMessage(false); // Hide redirect message when modal is opened
             }}
           >
             Open
           </button>
         </div>
+        {showRedirectMessage && (
+  <p className="text-center mt-4">
+    Please visit the <a className="text-blue-500" href="/my-account">My Account</a> page to view your appointments.
+  </p>
+)}
+
         <AnimatePresence>
           {showModal && (
             <div
               className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
-              onClick={() => setShowModal(false)}
+              onClick={handleCloseModal}
             >
               <motion.div
                 className="relative bg-white rounded-lg w-full h-full md:w-3/4 md:h-3/4"
@@ -52,7 +66,7 @@ function Appointment() {
                 )}
                 <button
                   className={`absolute top-2 left-1/2 md:top-[-2rem] md:left-auto md:right-[-2rem] transform md:transform-none -translate-x-1/2 w-8 h-8 md:w-10 md:h-10 flex items-center justify-center rounded-full bg-red-600 text-white focus:outline-none focus:ring focus:ring-offset-2 focus:ring-black`}
-                  onClick={() => setShowModal(false)}
+                  onClick={handleCloseModal}
                 >
                   X
                 </button>
