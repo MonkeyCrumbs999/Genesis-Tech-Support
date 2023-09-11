35642527802770e69739a6e0c98f5056f9690fd8
Max Burleigh <webwavebuilding@yahoo.com>
2023-09-10 21:11:19 -0700

    adjusted speed of mobile sidebar

--- client/src/components/MobileSidebar.js
+++ client/src/components/MobileSidebar.js
@@ -41,9 +41,10 @@ function MobileSidebar({ isOpen, toggleMenu }) {
         sidebarRef.current &&
         !sidebarRef.current.contains(event.target)
       ) {
-        setTimeout(() => toggleMenu(event), 0);
+        toggleMenu(event);  // Remove setTimeout
       }
     }
+    
 
     document.addEventListener("mousedown", handleClickOutside);
 
@@ -61,7 +62,7 @@ function MobileSidebar({ isOpen, toggleMenu }) {
           animate={isOpen ? "open" : "closed"}
           exit="closed"
           variants={sidebar}
-          transition={{ damping: 20, stiffness: 400 }}
+          transition={{ damping: 20, stiffness: 20 }}
           className={`fixed top-0 left-0 bottom-0 w-64 bg-white p-4 overflow-y-auto z-50 border-r-[3px] border-genesis-orange`}
           onClick={(event) => event.stopPropagation()}>
           <button onClick={toggleMenu} className="mb-4 ml-2 text-black">
--- client/src/components/my-account/AccountMainContent.js
+++ client/src/components/my-account/AccountMainContent.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 function AccountMainContent({ user, renderTuneupSection }) {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    axios.get('/user/appointments')
+      .then(response => {
+        setAppointments(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching appointments:', error);
+      });
+  }, []);
+
   return (
     <div className="w-full col-span-5 flex flex-col items-start justify-start sm:items-center">
       <h2 className="text-4xl text-center lg:text-start font-bold">
@@ -9,9 +22,21 @@ function AccountMainContent({ user, renderTuneupSection }) {
       <div className="mt-10 w-full max-w-xl px-8 py-6 bg-gray-100 border border-gray-200 shadow-md rounded-lg min-w-full">
         <div>
           <h2 className="text-3xl font-semibold">Your Upcoming Appointments</h2>
-          <p className="mt-2 text-sm text-gray-600">
-            No upcoming appointments scheduled yet.
-          </p>
+          {appointments.length > 0 ? (
+            appointments.map(appointment => (
+              <div key={appointment.id}>
+                <p>Status: {appointment.status}</p>
+                <p>Start at: {appointment.start_at}</p>
+                <p>Created at: {appointment.created_at}</p>
+                <p>Updated at: {appointment.updated_at}</p>
+                {/* Add more fields as needed */}
+              </div>
+            ))
+          ) : (
+            <p className="mt-2 text-sm text-gray-600">
+              No upcoming appointments scheduled yet.
+            </p>
+          )}
         </div>
         <div className="mt-10">
           <h2 className="text-3xl font-semibold">Your Previous Appointments</h2>
